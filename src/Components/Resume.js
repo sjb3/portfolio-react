6a404c724e119d68b801b2002295585c96cf71fb
Sung J Byun <sungjbyun@D-108-179-167-134.dhcp4.washington.edu>
2017-04-21 20:15:36 -0700

    work on resume part

---
+++ src/Components/Resume.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import React from 'react';
+import { render } from 'react-dom';
+
+import '../scss/base.scss';
+import '../scss/lib/_theme.scss';
+
+export const Resume = React.createClass ({
+  render() {
+    return (
+  <div class="resume-class">
+
+    <h1>Justin Byun</h1>
+    <h2 class="title"><em>Full-Stack Software Developer</em></h2>
+
+    <h3 class="manifesto"><em>As a former Biotech Researcher, I learned the beauty of analytical skills and value of critical thinking: the artistry of science.
+      Now, as a full-stack software developer, I’d like to add the artistry of science to programming to develop user-friendly, efficient and resource saving apps. </em></h3>
+
+
+  </div>
+    )
+  }
+});
