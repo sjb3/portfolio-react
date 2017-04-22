b4994291b8ae0d5c21fcbf71301606a0a5b34c49
Sung J Byun <sungjbyun@D-108-179-167-134.dhcp4.washington.edu>
2017-04-21 20:55:38 -0700

    class -> className

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -9,12 +9,12 @@ import '../scss/lib/_theme.scss';
 export class Resume extends React.Component {
   render() {
     return (
-  <div class="resume-class">
+  <div className="resume-class">
 
     <h1>Justin Byun</h1>
-    <h2 class="title"><em>Full-Stack Software Developer</em></h2>
+    <h2 className="title"><em>Full-Stack Software Developer</em></h2>
 
-    <h3 class="manifesto"><em>As a former Biotech Researcher, I learned the beauty of analytical skills and value of critical thinking: the artistry of science.
+    <h3 className="manifesto"><em>As a former Biotech Researcher, I learned the beauty of analytical skills and value of critical thinking: the artistry of science.
       Now, as a full-stack software developer, I’d like to add the artistry of science to programming to develop user-friendly, efficient and resource saving apps. </em></h3>
 
 
