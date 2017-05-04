9c2edd4ac69b769af0f0d26901e272c788f1d66d
Sung J Byun <sungjbyun@D-128-208-46-124.dhcp4.washington.edu>
2017-05-04 16:44:15 -0700

    clean code!

--- src/Components/NotFound404/NotFound404.style.js
+++ src/Components/NotFound404/NotFound404.style.js
@@ -6,13 +6,20 @@ import '../../scss/base.scss';
 import '../../scss/lib/_theme.scss';
 
 export const NotFoundBackground = styled.div`
-  position: absolute;
+  // position: absolute;
 
-  width: 100%;
-  height: 100%;
-  background-image: url(${require('../../pic/404.jpg')});
-  background-ize: 100%;
-  z-index: -99;
-  background-repeat: no-repeat;
-  background-position: center;
+  // width: 100%;
+  // height: 100%;
+  // background-image: url(${require('../../pic/404.jpg')});
+  // background-ize: 100%;
+  // z-index: -99;
+  // background-repeat: no-repeat;
+  // background-position: center;
+
+  // background-color: white;
+  // font-size: 2em;
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
+  // flex-wrap: wrap;
 `
