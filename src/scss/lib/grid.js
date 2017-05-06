d196cc024ce5533ed3289a7defd768f0aebac86b
Sung J Byun <sungjbyun@D-173-250-148-114.dhcp4.washington.edu>
2017-05-05 17:10:09 -0700

    fully responsive!

--- src/scss/lib/grid.js
+++ src/scss/lib/grid.js
@@ -1,7 +1,29 @@
-import styled from 'styled-components';
+'use strict';
 
-export const Container = styled.div `
+
+import styled, {} from 'styled-components';
+import media from './media';
+
+
+export const Div = styled.div`
+  ${({ marginBottom }) => marginBottom && css`
+    margin-bottom: ${marginBottom};
+    `}
+`;
+
+export const Container = styled(Div)`
   padding-left: 120px;
   padding-right: 120px;
-  padding-top: 30px
+  padding-top: 30px;
+  ${media.phone}`
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-top: 15px;
+    `}
+
+    ${media.tablet}`
+    padding-left: 30px;
+    padding-right: 30px;
+    padding-top: 15px;
+    `}
 `;
